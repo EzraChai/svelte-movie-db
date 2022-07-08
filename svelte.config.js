a9a3783eacbff4cab1847522d22085ef1ee1f3fe
Ezra Chai <juanzhe2@gmail.com>
2022-07-08 15:46:39 +0800

    build commit

--- svelte.config.js
+++ svelte.config.js
@@ -1,5 +1,5 @@
 import preprocess from 'svelte-preprocess';
-import adapter from '@sveltejs/adapter-node';
+import node from "@sveltejs/adapter-node";
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
@@ -9,7 +9,8 @@ const config = {
 		})
 	],
 	kit: {
-		adapter: adapter()
+		target: "#svelte",
+		adapter: node({env: {port: process.env.Port}}),
 	}
 };
 
