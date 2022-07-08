ba72079571e713a6c21cac5b068b89131636ba93
Ezra Chai <juanzhe2@gmail.com>
2022-07-08 15:33:06 +0800

    build commit

--- svelte.config.js
+++ svelte.config.js
@@ -1,12 +1,12 @@
-import adapter from '@sveltejs/adapter-auto';
-import preprocess from "svelte-preprocess";
+import preprocess from 'svelte-preprocess';
+import adapter from '@sveltejs/adapter-node';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	preprocess: [
 		preprocess({
-		postcss: true,
-		}),
+			postcss: true
+		})
 	],
 	kit: {
 		adapter: adapter()
